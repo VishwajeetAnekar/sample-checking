2828318009563a9c01f78e5e4e151c3f364ed1c5
Vishwajeet Anekar <86238855+VishwajeetAnekar@users.noreply.github.com>
2024-03-06 10:16:00 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -34,3 +34,33 @@ def unique_value_validation(csv_file1, csv_file2):
     result_df = pd.DataFrame(result_rows)
 
     return result_df, "Error: Common values found"
+
+
+def duplicate_keys_validation(df):
+    column_names = df.columns.tolist()
+    status_list = ["Success"] * len(column_names)
+    details_list = [""] * len(column_names)
+
+    for i, col_name in enumerate(column_names):
+        if f"{col_name}.1" in column_names[i + 1:]:
+            status_list[i] = "Success"
+            details_list[i] = "Duplicate Columns: Original"
+        elif '.1' in col_name:
+            index = column_names.index(col_name)
+            status_list[index] = "Failed"
+            details_list[index] = "Duplicate Column"
+
+    column_names = [col_name.split('.')[0] for col_name in column_names]
+
+    result_df = pd.DataFrame({
+        "Column Name": column_names,
+        "Status": status_list,
+        "Details": details_list
+    })
+
+    if result_df['Status'].eq('Success').all():
+        validation_result = "Success: No duplicate columns found"
+    else:
+        validation_result = result_df.to_html(index=False)
+
+    return result_df, validation_result
